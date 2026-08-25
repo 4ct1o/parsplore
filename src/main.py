ac9bdc0833ea58c01fc8181d06b5a9047e3752a0
Acti0ns <metesilahsor@gmail.com>
2026-08-25 17:08:01 +0300

    feat(main): add application class

--- src/main.py
+++ src/main.py
@@ -1,25 +1,44 @@
-import os
 import sys
 
-from dotenv import load_dotenv
 from PySide6.QtWidgets import QApplication, QMainWindow
 
 from ui.email_setup_window import EmailSetupWindow
 from ui.main_window import MainWindow
+from config.settings import load_settings
+from validators.email_validator import is_valid_email
+
+class Application(QApplication):
+    def __init__(self, argv):
+        super().__init__(argv)
+
+        self.window = None
+
+    def start(self):
+        settings = load_settings()
+        email = settings.get("email")
+
+        if is_valid_email(email):
+            self.show_main_window()
+        else:
+            self.show_email_setup_window()
+
+    def show_email_setup_window(self):
+        self.window = EmailSetupWindow()
+        self.window.email_saved.connect(self.show_main_window)
+        self.window.show()
+
+    def show_main_window(self):
+        if self.window:
+            self.window.close()
+
+        self.window = MainWindow()
+        self.window.show()
 
 def main():
-    load_dotenv()
 
-    app = QApplication(sys.argv)
+    app = Application(sys.argv)
 
-    email = os.getenv("USER_EMAIL")
-
-    if email:
-        window = MainWindow()
-    else:
-        window = EmailSetupWindow()
-
-    window.show()
+    app.start()
 
     sys.exit(app.exec())
 
