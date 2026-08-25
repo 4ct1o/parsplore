ee0f1f9085eaf5201a13927eda8cc8178b25c40a
Acti0ns <metesilahsor@gmail.com>
2026-08-25 17:03:05 +0300

    feat(config): add settings functions and email set

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,4 +1,12 @@
-APP_NAME = "parsplore()"
+import json
+from pathlib import Path
 
-WINDOW_WIDTH = 800
-WINDOW_HEIGHT = 600
+SETTINGS_FILE = Path(__file__).parent / "settings.json"
+
+def load_settings():
+    with SETTINGS_FILE.open('r', encoding='utf-8') as file:
+        return json.load(file)
+
+def save_settings(settings: dict) -> None:
+    with SETTINGS_FILE.open('w', encoding='utf-8') as file:
+        json.dump(settings, file, indent=4)
