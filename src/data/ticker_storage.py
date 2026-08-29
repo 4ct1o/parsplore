fd1152fa1b2332a0531c7a594c255fd22e95a946
Acti0ns <metesilahsor@gmail.com>
2026-08-29 14:12:56 +0300

    feat(data): add ticker storage functions

--- src/data/ticker_storage.py
+++ src/data/ticker_storage.py
@@ -1,1 +1,23 @@
 """Ticker storage module."""
+
+import json
+
+from src.clients.sec_client import get_sec_tickers, get_sec_ticker_last_modified
+
+def save_tickers(file: str = "data/tickers.json") -> None:
+    """
+    Save the tickers.
+
+    Args:
+        file (str): The path to the file to save the tickers to.
+    """
+
+    sec_tickers, last_modified = get_sec_tickers()
+
+    data = {
+        "last_modified": last_modified,
+        "tickers": sec_tickers,
+    }
+
+    with open(file, "w") as f:
+        json.dump(data, f, indent=2)
