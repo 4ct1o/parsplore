1d3298cfbcc03ede7a66fb57e287d61c76a461af
Acti0ns <metesilahsor@gmail.com>
2026-09-01 14:47:52 +0300

    feat(clients): add latest submissions function

--- src/clients/sec_client.py
+++ src/clients/sec_client.py
@@ -3,6 +3,7 @@
 import requests
 
 from src.config.settings import load_settings
+from src.services.sec_service import create_company_submission_link
 
 def get_headers() -> dict:
     """
@@ -55,3 +56,42 @@ def get_sec_ticker_last_modified() -> str | None:
     last_modified = response.headers.get("Last-Modified")
 
     return last_modified
+
+def get_latest_submissions(cik: str, limit: int = 10) -> dict:
+    """
+    Get the latest submissions for a given link.
+
+    Args:
+        link (str): The link to the SEC submissions page.
+    Returns:
+        dict: The latest submissions for the given link.
+    """
+
+    url = create_company_submission_link(cik)
+
+    headers = get_headers()
+
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+
+    data = response.json()
+
+    recent_filings = []
+
+    for key in range(min(limit, len(data['recent']))):
+        filing = {
+            'accessionNumber':       data['filings']['recent']['accessionNumber'][key],
+            'filingDate':            data['filings']['recent']['filingDate'][key],
+            'reportDate':            data['filings']['recent']['reportDate'][key],
+            'acceptanceDateTime':    data['filings']['recent']['acceptanceDateTime'][key],
+            'form':                  data['filings']['recent']['form'][key],
+            'fileNumber':            data['filings']['recent']['fileNumber'][key],
+            'primaryDocument':       data['filings']['recent']['primaryDocument'][key],
+            'primaryDocDescription': data['filings']['recent']['primaryDocDescription'][key],
+            'items':                 data['filings']['recent']['items'][key],
+            'isXBRL':                data['filings']['recent']['isXBRL'][key],
+            'isInlineXBRL':          data['filings']['recent']['isInlineXBRL'][key],
+        }
+        recent_filings.append(filing)
+
+    return recent_filings
