de9894c4aa8acc9cab7b42e9b05ff85aba1b5581
Acti0ns <metesilahsor@gmail.com>
2026-09-03 17:54:56 +0300

    feat(ui): add background task to main_window

--- src/ui/main_window.py
+++ src/ui/main_window.py
@@ -10,9 +10,14 @@ from PySide6.QtWidgets import (
     QWidget,
 )
 
-from PySide6.QtCore import Qt
+from PySide6.QtCore import (
+    Qt,
+    QThread,
+    Signal,
+)
 
 from config.settings import load_settings
+from tasks.sec_tasks import SECTickerTask
 
 class MainWindow(QMainWindow):
     """Main application window."""
@@ -47,3 +52,31 @@ class MainWindow(QMainWindow):
         layout.addWidget(form_widget, alignment=Qt.AlignCenter)
 
         self.setCentralWidget(central_widget)
+
+        self.search_button.clicked.connect(self.handle_search)
+
+        self.start_sec_ticker_task()
+
+    search_term = Signal(str)
+
+    def handle_search(self):
+        """Emit the search term signal."""
+        term = self.search_input.text()
+        if term:
+            self.search_term.emit(term)
+
+    def start_sec_ticker_task(self):
+        """Start the SEC ticker task."""
+
+        self.sec_ticker_task = SECTickerTask()
+        self.sec_ticker_thread = QThread()
+
+        self.sec_ticker_task.moveToThread(self.sec_ticker_thread)
+
+        self.sec_ticker_thread.started.connect(self.sec_ticker_task.run)
+        self.sec_ticker_task.finished.connect(self.sec_ticker_thread.quit)
+        self.sec_ticker_task.finished.connect(self.sec_ticker_task.deleteLater)
+        self.sec_ticker_thread.finished.connect(self.sec_ticker_thread.deleteLater)
+
+        self.sec_ticker_thread.start()
+
--- src/ui/search_window.py
+++ src/ui/search_window.py
@@ -1,7 +1,9 @@
-"""Search window."""
+"""Search windows."""
 
 from PySide6.QtWidgets import (
-    QVBoxLayout, 
+    QVBoxLayout,
+    QHBoxLayout,
+    QWidget,
     QLabel
 )
 
@@ -10,7 +12,29 @@ from PySide6.QtCore import (
     Signal
 )
 
-from src.services.sec_service import search_ticker
+from config.settings import load_settings
+from services.sec_service import search_company
+
+class CompanySearchWindow(QWidget):
+    """Company search window."""
+    def __init__(self, search_term: str):
+        super().__init__()
+
+        settings = load_settings()
+
+        self.setWindowTitle(f"{settings.get('app_name', 'parsplore()')} - Company Search")
+        self.resize(settings.get('window_width', 800), settings.get('window_height', 600))
+
+        self.company_title_label = QLabel('Company (Ticker):')
+
+        results = search_company(search_term, limit=10)
+
+        for company in results:
+            title, ticker, cik, score = company
+            company_info_label = QLabel(f"{title} ({ticker})")
+            company_info_label.setAlignment(Qt.AlignLeft)
+            self.layout().addWidget(company_info_label)
+        
 
 
 
